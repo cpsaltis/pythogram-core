7a5161739b9348e577f484143e56a37f327104c6
Christos Psaltis <cpsaltis@unweb.me>
2012-11-29 15:37:45 +0200

    Add initial resize and rotate, no tests yet

--- src/gramcore/transformations/geometric.py
+++ src/gramcore/transformations/geometric.py
@@ -1,12 +1,37 @@
+"""Geometric transformations on arrays. They are more useful in the context
+that these arrays are in fact images.
 """
-"""
+from skimage import transform
 
 
 def resize(parameters):
-    pass
+    """Resizes input to match a certain size.
+
+    Check http://scikit-image.org/docs/dev/api/skimage.transform.html#resize
+
+    :param parameters['data'][0]: array to resize
+    :type parameters['data'][0]: numpy.array
+    :param parameters['output_shape']: size of the output
+    :type parameters['data'][1]: tuple
+
+    :return: numpy.array
+    """
+    return transform.resize(parameters['data'][0], parameters['output_shape'])
 
 
 def rotate(parameters):
-    pass
+    """Rotates input anti-clockwise around its center.
+
+    Check http://scikit-image.org/docs/dev/api/skimage.transform.html#rotate
+
+    :param parameters['data'][0]: array to rotate
+    :type parameters['data'][0]: numpy.array
+    :param parameters['angle']: rotation angle in degrees
+    :type parameters['angle']: float
+    :param parameters['resize']: expand
+    :type parameters['resize']: bool
+
+    :return: numpy.array
+    """
 
 
